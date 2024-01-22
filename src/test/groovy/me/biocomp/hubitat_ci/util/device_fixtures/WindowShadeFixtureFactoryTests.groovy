eb85556b40ee15057a75de96b590665956ee0ff2
Joel Wetzel <jwetzel@affirma.com>
2024-01-22 10:06:38 -0600

    Adds a fixture factory for creating mock window shade devices.

---
+++ src/test/groovy/me/biocomp/hubitat_ci/util/device_fixtures/WindowShadeFixtureFactoryTests.groovy
@@ -0,0 +1,135 @@
+package me.biocomp.hubitat_ci.util.device_fixtures
+
+import me.biocomp.hubitat_ci.api.app_api.AppExecutor
+import me.biocomp.hubitat_ci.api.common_api.DeviceWrapper
+import me.biocomp.hubitat_ci.api.common_api.Log
+import me.biocomp.hubitat_ci.util.device_fixtures.WindowShadeFixtureFactory
+
+import spock.lang.Specification
+
+class WindowShadeFixtureFactoryTests extends Specification {
+    def log = Mock(Log)
+
+    def appExecutor = Mock(AppExecutor) {
+        _*getLog() >> log
+    }
+
+    void "Shade can close"() {
+        given:
+        def fixture = WindowShadeFixtureFactory.create('n')
+        fixture.initialize(appExecutor, [windowShade: "partially open", position: 50])
+
+        when:
+        fixture.close()
+
+        then:
+        1*appExecutor.sendEvent(fixture, [name: "position", value: 0])
+        1*appExecutor.sendEvent(fixture, [name: "windowShade", value: "closed"])
+        fixture.state.position == 0
+        fixture.state.windowShade == "closed"
+    }
+
+    void "Shade can open"() {
+        given:
+        def fixture = WindowShadeFixtureFactory.create('n')
+        fixture.initialize(appExecutor, [windowShade: "partially open", position: 50])
+
+        when:
+        fixture.open()
+
+        then:
+        1*appExecutor.sendEvent(fixture, [name: "position", value: 100])
+        1*appExecutor.sendEvent(fixture, [name: "windowShade", value: "open"])
+        fixture.state.position == 100
+        fixture.state.windowShade == "open"
+    }
+
+    void "Shade can set position to closed"() {
+        given:
+        def fixture = WindowShadeFixtureFactory.create('n')
+        fixture.initialize(appExecutor, [windowShade: "partially open", position: 50])
+
+        when:
+        fixture.setPosition(0)
+
+        then:
+        1*appExecutor.sendEvent(fixture, [name: "position", value: 0])
+        1*appExecutor.sendEvent(fixture, [name: "windowShade", value: "closed"])
+        fixture.state.position == 0
+        fixture.state.windowShade == "closed"
+    }
+
+    void "Shade can set position to open"() {
+        given:
+        def fixture = WindowShadeFixtureFactory.create('n')
+        fixture.initialize(appExecutor, [windowShade: "partially open", position: 50])
+
+        when:
+        fixture.setPosition(100)
+
+        then:
+        1*appExecutor.sendEvent(fixture, [name: "position", value: 100])
+        1*appExecutor.sendEvent(fixture, [name: "windowShade", value: "open"])
+        fixture.state.position == 100
+        fixture.state.windowShade == "open"
+    }
+
+    void "Shade can set position to partially open"() {
+        given:
+        def fixture = WindowShadeFixtureFactory.create('n')
+        fixture.initialize(appExecutor, [windowShade: "open", position: 100])
+
+        when:
+        fixture.setPosition(50)
+
+        then:
+        1*appExecutor.sendEvent(fixture, [name: "position", value: 50])
+        1*appExecutor.sendEvent(fixture, [name: "windowShade", value: "partially open"])
+        fixture.state.position == 50
+        fixture.state.windowShade == "partially open"
+    }
+
+    void "Shade can start changing to open"() {
+        given:
+        def fixture = WindowShadeFixtureFactory.create('n')
+        fixture.initialize(appExecutor, [windowShade: "closed", position: 0])
+
+        when:
+        fixture.startPositionChange("open")
+
+        then:
+        1*appExecutor.sendEvent(fixture, [name: "position", value: 100])
+        1*appExecutor.sendEvent(fixture, [name: "windowShade", value: "open"])
+        fixture.state.position == 100
+        fixture.state.windowShade == "open"
+    }
+
+    void "Shade can start changing to closed"() {
+        given:
+        def fixture = WindowShadeFixtureFactory.create('n')
+        fixture.initialize(appExecutor, [windowShade: "open", position: 100])
+
+        when:
+        fixture.startPositionChange("close")
+
+        then:
+        1*appExecutor.sendEvent(fixture, [name: "position", value: 0])
+        1*appExecutor.sendEvent(fixture, [name: "windowShade", value: "closed"])
+        fixture.state.position == 0
+        fixture.state.windowShade == "closed"
+    }
+
+    void "If given an invalid direction, shade does nothing"() {
+        given:
+        def fixture = WindowShadeFixtureFactory.create('n')
+        fixture.initialize(appExecutor, [windowShade: "partially open", position: 50])
+
+        when:
+        fixture.startPositionChange("invalid")
+
+        then:
+        0*appExecutor.sendEvent(fixture, _)
+        fixture.state.position == 50
+        fixture.state.windowShade == "partially open"
+    }
+}
