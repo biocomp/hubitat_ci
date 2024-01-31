57c66c95b4820fb1903272487f571cbf5be64acc
Joel Wetzel <jwetzel@affirma.com>
2024-01-31 10:12:35 -0600

    Makes TimeKeeper thread-safe and adds observability.

--- src/main/groovy/me/biocomp/hubitat_ci/util/TimeKeeper.groovy
+++ src/main/groovy/me/biocomp/hubitat_ci/util/TimeKeeper.groovy
@@ -2,6 +2,9 @@ package me.biocomp.hubitat_ci.util
 
 import groovy.time.*
 
+import groovy.transform.Synchronized
+import java.util.concurrent.CopyOnWriteArrayList
+
 /**
 * TimeKeeperDate is just a Date that we can override the constructor for, without impacting
 * the base Date constructor.
@@ -19,12 +22,18 @@ class TimeKeeperDate extends Date {
 class TimeKeeper {
     private Date internalDate = null
 
+    private final CopyOnWriteArrayList<TimeChangedListener> timeChangedListeners
+
+    private final timekeeperLock = new Object()
+
     TimeKeeper() {
         internalDate = new Date()
+        timeChangedListeners = new CopyOnWriteArrayList<TimeChangedListener>()
     }
 
     TimeKeeper(Date startingDate) {
         internalDate = startingDate
+        timeChangedListeners = new CopyOnWriteArrayList<TimeChangedListener>()
     }
 
     /**
@@ -50,19 +59,66 @@ class TimeKeeper {
         internalDate = newDate
     }
 
+    def advanceMillis(int millis) {
+        def oldDate = internalDate
+        internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(0, 0, 0, 0, millis))
+        fireTimeChangedEvent(oldDate, internalDate)
+    }
+
     def advanceSeconds(int seconds) {
+        def oldDate = internalDate
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(0, 0, 0, seconds, 0))
+        fireTimeChangedEvent(oldDate, internalDate)
     }
 
     def advanceMinutes(int minutes) {
+        def oldDate = internalDate
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(0, 0, minutes, 0, 0))
+        fireTimeChangedEvent(oldDate, internalDate)
     }
 
     def advanceHours(int hours) {
+        def oldDate = internalDate
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(0, hours, 0, 0, 0))
+        fireTimeChangedEvent(oldDate, internalDate)
     }
 
     def advanceDays(int days) {
+        def oldDate = internalDate
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(days, 0, 0, 0, 0))
+        fireTimeChangedEvent(oldDate, internalDate)
     }
+
+    @Synchronized("timekeeperLock")
+    def addListener(TimeChangedListener listener) {
+        timeChangedListeners.add(listener)
+    }
+
+    @Synchronized("timekeeperLock")
+    def removeListener(TimeChangedListener listener) {
+        timeChangedListeners.remove(listener)
+    }
+
+    @Synchronized("timekeeperLock")
+    def fireTimeChangedEvent(Date oldTime, Date newTime) {
+        TimeChangedEvent event = new TimeChangedEvent(this, oldTime, newTime)
+        timeChangedListeners.each { listener ->
+            listener.timeChangedEventReceived(event)
+        }
+    }
+}
+
+class TimeChangedEvent extends EventObject {
+    Date oldTime
+    Date newTime
+
+    TimeChangedEvent(Object source, Date oldTime, Date newTime) {
+        super(source)
+        this.oldTime = oldTime
+        this.newTime = newTime
+    }
+}
+
+interface TimeChangedListener {
+    void timeChangedEventReceived(TimeChangedEvent event)
 }
