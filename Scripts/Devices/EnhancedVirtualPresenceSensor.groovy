c3c8e4c2e0a412aba21dfd098cbdff138fc5f566
Joel Wetzel <jwetzel@affirma.com>
2024-03-13 12:08:44 -0500

    Clean up tabs/spaces

--- Scripts/Devices/EnhancedVirtualPresenceSensor.groovy
+++ Scripts/Devices/EnhancedVirtualPresenceSensor.groovy
@@ -2,15 +2,15 @@
 * A virtual presence sensor that also implements the Switch capability, and links their state.
 **/
 metadata {
-        definition (name: "Enhanced Virtual Presence Sensor", namespace: "hubitat_ci", author: "hubitat_ci") {
+	definition (name: "Enhanced Virtual Presence Sensor", namespace: "hubitat_ci", author: "hubitat_ci") {
 		capability "Actuator"
-        capability "Switch"
-        capability "PresenceSensor"
+		capability "Switch"
+		capability "PresenceSensor"
 		capability "Sensor"
 
 		command "arrived"
 		command "departed"
-    }
+	}
 }
 
 def parse(String description) {
@@ -24,15 +24,15 @@ def arrived() {
 
 
 def departed() {
-    off()
+	off()
 }
 
 def on() {
 	sendEvent(name: "switch", value: "on")
-    sendEvent(name: "presence", value: "present")
+	sendEvent(name: "presence", value: "present")
 }
 
 def off() {
 	sendEvent(name: "switch", value: "off")
-    sendEvent(name: "presence", value: "not present")
+	sendEvent(name: "presence", value: "not present")
 }
